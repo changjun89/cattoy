b654cfd81aab682e4b04abd9097b307f35d0e278
changjun89 <leechang0423@naver.com>
2019-10-22 20:00:01 +0900

    test GreetingController

--- src/test/java/com/changjun/cattoy/controller/GreetingControllerTest.java
+++ src/test/java/com/changjun/cattoy/controller/GreetingControllerTest.java
@@ -1,11 +1,32 @@
 package com.changjun.cattoy.controller;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(GreetingController.class)
 public class GreetingControllerTest {
 
-  @Test
-  public void name() {
+    @Autowired
+    private MockMvc mockMvc;
 
-  }
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("Hello World")))
+                .andExpect(content().string(Matchers.containsString("changjun")));
+    }
 }
