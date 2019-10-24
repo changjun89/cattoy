fbb39eefe5f16f29e92fc37bff7b5ef780c11bf5
changjun89 <leechang0423@naver.com>
2019-10-24 21:24:27 +0900

    feat "ProductRepository extends JpaRepository"

--- src/main/java/com/changjun/cattoy/domain/ProductRepository.java
+++ src/main/java/com/changjun/cattoy/domain/ProductRepository.java
@@ -1,13 +1,14 @@
 package com.changjun.cattoy.domain;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findAll();
 
-    void save(String name);
+    Product save(Product product);
 }
