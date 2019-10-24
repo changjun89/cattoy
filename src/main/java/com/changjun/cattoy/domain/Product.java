a37245df05f187e80b473c60bc144a90e204e07c
changjun89 <leechang0423@naver.com>
2019-10-24 21:26:06 +0900

    feat "set Product Entity"

--- src/main/java/com/changjun/cattoy/domain/Product.java
+++ src/main/java/com/changjun/cattoy/domain/Product.java
@@ -5,19 +5,26 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.text.NumberFormat;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Product {
     private static final String DEFAULTS_IMG_URL = "";
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String maker;
     private int price;
-
     private String imgUrl;
 
     public String getPriceWithComma() {
