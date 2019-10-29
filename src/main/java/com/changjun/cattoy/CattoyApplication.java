16a4fda28432e8d8b192eea98e00a17a695def4b
changjun89 <leechang0423@naver.com>
2019-10-30 01:37:53 +0900

    feat "Dozer Mapper Bean"

--- src/main/java/com/changjun/cattoy/CattoyApplication.java
+++ src/main/java/com/changjun/cattoy/CattoyApplication.java
@@ -1,13 +1,21 @@
 package com.changjun.cattoy;
 
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CattoyApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(CattoyApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(CattoyApplication.class, args);
+    }
+
+    @Bean
+    public Mapper mapper() {
+        return DozerBeanMapperBuilder.buildDefault();
+    }
 
 }
