568b5ca75eb6b0c16c3361194b46b144580d6927
changjun89 <leechang0423@naver.com>
2019-11-07 20:03:37 +0900

    feat "user signUp"

--- src/main/java/com/changjun/cattoy/controller/TokenController.java
+++ src/main/java/com/changjun/cattoy/controller/TokenController.java
@@ -1,7 +1,11 @@
 package com.changjun.cattoy.controller;
 
+import com.changjun.cattoy.application.UserService;
+import com.changjun.cattoy.domain.User;
+import com.changjun.cattoy.dto.SignDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -10,8 +14,18 @@ import java.net.URISyntaxException;
 @RestController
 public class TokenController {
 
+    private final UserService userService;
+
+    public TokenController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping("/token")
-    public ResponseEntity signIn() throws URISyntaxException {
+    public ResponseEntity signIn(@RequestBody SignDto signDto) throws URISyntaxException {
+        User user = userService.authenticate(signDto.getEmail(), signDto.getPassword());
+        if(user ==null) {
+            return ResponseEntity.notFound().build();
+        }
         URI uri = new URI("/");
         return ResponseEntity.created(uri).build();
     }
--- src/test/java/com/changjun/cattoy/controller/TokenControllerTest.java
+++ src/test/java/com/changjun/cattoy/controller/TokenControllerTest.java
@@ -1,6 +1,7 @@
 package com.changjun.cattoy.controller;
 
 import com.changjun.cattoy.application.UserService;
+import com.changjun.cattoy.domain.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,10 +31,26 @@ public class TokenControllerTest {
     private UserService userService;
 
     @Test
-    public void signIn() throws Exception {
+    public void signInWithValidAttributes() throws Exception {
+        User mockUser = User.builder()
+                .email("leechang0423@naver.com")
+                .password("password")
+                .build();
+        given(userService.authenticate("leechang0423@naver.com","psssword")).willReturn(mockUser);
         mockMvc.perform(post("/token")
         .contentType(MediaType.APPLICATION_JSON_UTF8)
         .content("{\"email\":\"leechang0423@naver.com\",\"password\":\"password\"}"))
                 .andExpect(status().isCreated());
+        verify(userService).authenticate(any(), any() );
+    }
+
+    @Test
+    public void signInWithInValidAttributes() throws Exception {
+        given(userService.authenticate("leechang0423@naver.com","psssword")).willReturn(null);
+        mockMvc.perform(post("/token")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content("{\"email\":\"leechang0423@naver.com\",\"password\":\"password\"}"))
+                .andExpect(status().isNotFound());
+        verify(userService).authenticate(any(), any() );
     }
 }
