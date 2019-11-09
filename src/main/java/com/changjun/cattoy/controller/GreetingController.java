6a7853903607fa0da9e2eb89d10271f3d52d94bc
changjun89 <leechang0423@naver.com>
2019-11-09 21:43:37 +0900

    feat "Custom Authentication"

--- src/main/java/com/changjun/cattoy/controller/GreetingController.java
+++ src/main/java/com/changjun/cattoy/controller/GreetingController.java
@@ -2,7 +2,6 @@ package com.changjun.cattoy.controller;
 
 import com.changjun.cattoy.application.GreetingService;
 import com.changjun.cattoy.dto.GreetingDto;
-import io.jsonwebtoken.Claims;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,8 +22,7 @@ public class GreetingController {
 
         // 인증 객체가 있다면 이름을 바꿔줍니다.
         if (authentication != null) {
-            Claims claims = (Claims) authentication.getPrincipal();
-            myName = claims.get("name", String.class);
+            myName = authentication.getName();
         }
 
         GreetingDto greeting = new GreetingDto();
